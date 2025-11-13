e54218c04761b8510335f641ff3485a946afd9c1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2025-11-13 13:52:35 +0100

    Yield first try :)

--- YieldReturn/Program.cs
+++ YieldReturn/Program.cs
@@ -4,12 +4,30 @@
     {
         static void Main(string[] args)
         {
-            var numbers = GetNumbers();
+            //var numbers = GetNumbers();
+            //IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+            //while (enumerator.MoveNext())
+            //{
+            //    var item = enumerator.Current;
+            //    Console.WriteLine(item);
+            //}
+
+            foreach (var item in GetNumbers())
+            {
+                Console.WriteLine(item);
+            }
         }
 
         public static IEnumerable<int> GetNumbers()
         {
-            return new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //return new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+          
         }
     }
 }
