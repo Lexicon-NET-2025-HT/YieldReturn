dfb116bcd255741eb8536b9d09c192dfd5095adc
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2025-11-13 13:07:25 +0100

    Add project files.

---
+++ YieldReturn/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace YieldReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = GetNumbers();
+        }
+
+        public static IEnumerable<int> GetNumbers()
+        {
+            return new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+    }
+}
